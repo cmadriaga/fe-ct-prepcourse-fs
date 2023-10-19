c44936f0fc232514831324212ee51a88656a9910
Carlos Madriaga <52353240+cmadriaga@users.noreply.github.com>
2023-10-19 09:46:38 -0300

    Update Ejercicio 03.js
    modificado 19-10

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,81 +5,66 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
-   if(x==y){
-      z = true;
+   if(x===y){
+      return true;
+   }else{
+      return false;
    }
-   else {
-      z = false;
-   }
-   return z;
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
-let sstr1 = str1;
-let sst2 = str2;   
-     if( sstr1.length == sstr2.length ){
-      z = true;
+   if(str1.length == str2.length){
+      return true;
+   }else {
+      return false;
    }
-   else {
-      z = false;
-   }
-   return z;
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
-     if( num < 90 ){
-      z = true;
+   if( num < 90){
+      return true;
+   } else {
+      return false;
    }
-   else {
-      z = false;
-   }
-   return z;
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
-     if(num > 50){
-      z = true;
+   if( num > 50){
+      return true;
+   } else {
+      return false;
    }
-   else {
-      z = false;
-   }
-   return z;
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
-     if((x % 2) == 0){
-      z = true;
+   if( num % 2 === 0){
+      return true;
+   } else {
+      return false;
    }
-   else {
-      z = false;
-   }
-   return z;
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
-  if((x%y) != 0 ){
-      z = true;
+   if( num%2 ==! 0){
+      return true;
+   } else {
+      return false;
    }
-   else {
-      z = false;
-   }
-   return z;
-   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
